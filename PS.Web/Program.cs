76babd7be8a61148a8f79c17ded0d04bf497d707
Skander Gasmi <49345776+SkanderGasmi@users.noreply.github.com>
2023-10-11 02:58:40 +0100

    setting up front end files(html css javascript etc)

---
+++ PS.Web/Program.cs
@@ -0,0 +1,35 @@
+using PS.Data.Infrastructures;
+using PS.Domain;
+using PS.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IProductService, ProductService>().
+AddScoped<ICategoryService, CategoryService>().
+AddScoped<IUnitOfWork, UnitOfWork>().
+AddSingleton<IDataBaseFactory, DatabaseFactory>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
